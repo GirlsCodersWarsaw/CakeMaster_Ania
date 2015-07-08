9fd9f453d2b4287777cc9f150fa4e9cb804a42a2
Ana <czarrrna@gmail.com>
2015-07-08 15:22:38 +0200

    fixing validations

--- app/models/cake.rb
+++ app/models/cake.rb
@@ -1,6 +1,6 @@
 class Cake < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 3, maximum: 30 }
-  validates :kind, presence: true,
+  validates :kind, presence: true
   validates :description, presence: true, length: { minimum: 10, maximum: 5000 }
 
 end
