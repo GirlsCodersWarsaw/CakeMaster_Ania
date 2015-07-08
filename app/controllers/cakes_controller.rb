772c52eb76d0d0c481185725892a6070fd39a6e3
Ana <czarrrna@gmail.com>
2015-07-08 16:27:55 +0200

    some changes for issue #4

--- app/controllers/cakes_controller.rb
+++ app/controllers/cakes_controller.rb
@@ -2,5 +2,16 @@ class CakesController < ApplicationController
   def index
     @cakes = Cake.all
   end
+
+  def new
+    @cake = Cake.new
+  end
+
+  def create
+    @cake = Cake.new
+    if @cake.save
+      redirect_to @cake
+    end
+    end
 end
 
