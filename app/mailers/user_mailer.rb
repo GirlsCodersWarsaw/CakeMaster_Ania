017558cc2e8f0ddc80bfcd9de8f9ca3ac0534865
Ana <czarrrna@gmail.com>
2015-08-06 16:12:38 +0200

    fixing user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
   default from: 'czarrrna@gmail.com'
 
-  def welcome_email(user)
-    @user = user
+  def welcome_email(user_id)
+    @user = User.find(user_id)
     @url  = 'www.google.com'
     mail(to: @user.email, subject: 'Welcome to CakeMaster')
   end
