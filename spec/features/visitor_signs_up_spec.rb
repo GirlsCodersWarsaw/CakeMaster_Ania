0b6cac0b2c9dfb0395b0f7d0687fb31afe17f220
Ana <czarrrna@gmail.com>
2015-08-19 16:16:41 +0200

    sign_up feature spec with capybara

--- spec/features/visitor_signs_up_spec.rb
+++ spec/features/visitor_signs_up_spec.rb
@@ -1,28 +1,30 @@
 require 'spec_helper'
+require 'rails_helper'
 
 feature 'Visitor signs up' do
   scenario 'with valid email and password' do
     sign_up_with 'valid@example.com', 'password'
 
-    expect(page).to have_content('Sign out')
+    expect(page).to have_content('Logout')
   end
 
   scenario 'with invalid email' do
     sign_up_with 'invalid_email', 'password'
 
-    expect(page).to have_content('Sign in')
+    expect(page).to have_content('Sign up')
   end
 
   scenario 'with blank password' do
     sign_up_with 'valid@example.com', ''
 
-    expect(page).to have_content('Sign in')
+    expect(page).to have_content('Sign up')
   end
 
   def sign_up_with(email, password)
-    visit sign_up_path
+    visit '/users/sign_up'
     fill_in 'Email', with: email
     fill_in 'Password', with: password
+    fill_in 'Password confirmation', with: password
     click_button 'Sign up'
   end
 end
