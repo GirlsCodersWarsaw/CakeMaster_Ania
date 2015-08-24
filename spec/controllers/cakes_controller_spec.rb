5d6426ec1cd6ae37ebed015e25f761c632406012
Ana <czarrrna@gmail.com>
2015-08-24 12:56:48 +0200

    spec controller WiP

--- spec/controllers/cakes_controller_spec.rb
+++ spec/controllers/cakes_controller_spec.rb
@@ -5,47 +5,31 @@ require 'devise'
 describe CakesController do
   login_user
   context "shows a list of cakes" do
-    it "gets :index view" do
-    get :index
-    end
     it "renders index template" do
-      render_template(:index)
+      get :index
+      expect(response).to render_template(:index)
+    end
   end
   context "shows creating new cake page" do
-    it "gets :new view" do
-    get :new
-    end
     it "renders new template" do
-      render_template(:new)
+      get :new
+      expect(response).to render_template(:new)
     end
   end
-  end
-  end
-  describe CakesController do
-    login_user
-    context "with valid attributes" do
-      it "creates a new cake" do
-        cake = Cake.new(name: "sernik", kind: "tasty", description: "sernik sernik sernik")
-        cake.save
-      end
-      it "redirects to list of cakes" do
-        cake = Cake.new(name: "sernik", kind: "tasty", description: "sernik sernik sernik")
-        response.should redirect_to root_path
-      end
+  context "create cake with valid attributes" do
+    it "saves a new cake" do
+      cake=create(:cake)
+      post :create, session: { name: 'sernik', kind: 'tasty', description: 'sernik sernik sernik' }
+      expect(response).to redirect_to(root_path)
     end
-=begin
+  end
 
-    context "with invalid attributes" do
-      it "does not save the new contact" do
-        expect{
-          post :create, contact: Factory.attributes_for(:invalid_contact)
-        }.to_not change(Contact,:count)
-      end
-
-      it "re-renders the new method" do
-        post :create, contact: Factory.attributes_for(:invalid_contact)
-        response.should render_template :new
-      end
+  context "with invalid attributes" do
+    it "does not save the new contact" do
+      cake=create(:cake)
+      expect{
+        post :create, session: { name: 'sernik', kind: 'tasty', description: 'sernik sernik sernik' }
+      }.to_not change(Cake,:count)
     end
-=end
   end
+end
--- spec/integration/home_spec.rb
+++ spec/integration/home_spec.rb
@@ -4,6 +4,6 @@ require 'rails_helper'
 describe 'home page' do
   it 'welcomes the user' do
     visit '/'
-    page.should have_content('Log in')
+    expect(page).to have_content('Log in')
   end
 end
